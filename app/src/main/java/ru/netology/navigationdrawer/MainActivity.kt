908a00fb2802426c3c72456a8689fa7e67383cf1
text-code <dssn2102@gmail.com>
2022-10-22 09:50:35 +0900

    Initial commit

---
+++ app/src/main/java/ru/netology/navigationdrawer/MainActivity.kt
@@ -0,0 +1,26 @@
+package ru.netology.navigationdrawer
+
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.navigationdrawer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            menuItem.isChecked = true
+            binding.drawerLayout.close()
+            true
+        }
+    }
+}
